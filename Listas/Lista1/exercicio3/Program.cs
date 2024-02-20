b2c815fdb6e8f6947cc80888ddbdc52d86c2a80a
Samuel-Junior21 <juniorehigor553@gmail.com>
2024-02-20 20:31:57 -0300

    Lista 1

---
+++ Listas/Lista1/exercicio3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program 
+{
+  public static void Main (string[] args)
+  {
+    Console.WriteLine ("Digite o valor em centavos: ");
+    int valor = int.Parse(Console.ReadLine());
+
+    int reais = valor / 100;
+    int centavos = valor % 100;
+
+
+
+    Console.WriteLine($"O total em dinheiro é: {reais} real(is) e {centavos} centavo(s). ");
+    
+
+ 
+    
+
+    
+    
+
+
+
+
+
+
+
+
+    
+  }
+}
