38896adb548f594c1da4c289ffd6676bcf8dd792
Samuel-Junior21 <juniorehigor553@gmail.com>
2024-02-20 21:02:41 -0300

    Lista 1

--- Listas/Lista1/exercicio1/Program.cs
+++ Listas/Lista1/exercicio1/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!"); 
+﻿ using System;
+
+class Program 
+{
+  public static void Main (string[] args)
+  {
+    Console.WriteLine ("Digite a nota do 1° BIMESTRE: ");
+    double valor = double.Parse(Console.ReadLine());
+    
+
+    Console.WriteLine ("Digite a nota do 2° BIMESTRE: ");
+    double valor = double.Parse(Console.ReadLine());
+
+
+    Console.WriteLine ("Digite a nota do 3° BIMESTRE: ");
+    double valor = double.Parse(Console.ReadLine());
+
+
+    Console.WriteLine ("Digite a nota do 4° BIMESTRE: ");
+    double valor = double.Parse(Console.ReadLine());
+
+
+    double media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+
+
+
+    Console.WriteLine($"Sua média é de: {media}. ");
+    
+
+ 
+    
+
+    
+    
+
+
+
+
+
+
+
+
+    
+  }
+}
--- Listas/Lista1/exercicio2/Program.cs
+++ Listas/Lista1/exercicio2/Program.cs
@@ -1,1 +1,38 @@
-﻿
+﻿using System;
+
+class Program 
+{
+  public static void Main (string[] args)
+  {
+    Console.WriteLine ("Digite o valor da base do triângulo: ");
+   double baseTriangulo = double.Parse(Console.ReadLine());
+
+   Console.WriteLine ("Digite o valor da altura do triângulo: ");
+   double alturaTriangulo = double.Parse(Console.ReadLine());
+
+   double valortotal = (baseTriangulo * alturaTriangulo) / 2;
+
+   bool analista = (valortotal > 20);
+
+
+
+    Console.WriteLine($"A área do triângulo é maior que 20? {analista}.");
+    
+
+ 
+    
+
+    
+    
+
+
+
+
+
+
+
+
+    
+  }
+}
+
